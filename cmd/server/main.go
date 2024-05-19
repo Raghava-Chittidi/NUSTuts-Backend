85ed145e867f7d1ef8dfff2add1456440be503b7
= <yeohchongyics@gmail.com>
2024-05-20 00:19:21 +0800

    fix: add removed db connection

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -2,29 +2,29 @@ package main
 
 import (
 	"NUSTuts-Backend/internal/application"
+	"NUSTuts-Backend/internal/database"
+	"NUSTuts-Backend/internal/util"
 	"context"
 	"log"
 )
 
 func main() {
-	// // Connect to db
-	// err := database.Connect()
-	// if err != nil {
-	// 	// log.Fatalln(err)
-	// 	log.Print(err)
-	// }
+	// Connect to db
+	err := database.Connect()
+	if err != nil {
+		log.Fatalln(err)
+	}
 
-	// // AutoMigrate db models
-	// err = util.Migrate()
-	// if err != nil {
-	// 	// log.Fatalln("Failed to migrate models!", err)
-	// 	log.Print("Failed to migrate models!", err)
-	// }
+	// AutoMigrate db models
+	err = util.Migrate()
+	if err != nil {
+		log.Fatalln("Failed to migrate models!", err)
+	}
 
 	// Start server
 	app := application.New()
-	err := app.Start(context.TODO())
-	if err != nil {
+	serverErr := app.Start(context.TODO())
+	if serverErr != nil {
 		log.Fatal(err)
 	}
 }
