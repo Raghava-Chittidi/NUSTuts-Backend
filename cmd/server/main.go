fb74c635b37edbfa9f8625a012374e4a6b228e6a
= <yeohchongyics@gmail.com>
2024-05-19 17:14:13 +0800

    feat: add test  chi router, log middleware

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -3,6 +3,9 @@ package main
 import (
 	"log"
 	"net/http"
+
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
 )
 
 func main() {
@@ -21,9 +24,14 @@ func main() {
 	// }
 
 	// Start server
+	router := chi.NewRouter()
+	router.Use(middleware.Logger)
+
+	router.Get("/hello", basicHandler)
+
 	server := &http.Server{
 		Addr:    ":3000",
-		Handler: http.HandlerFunc(basicHandler),
+		Handler: router,
 	}
 
 	serverErr := server.ListenAndServe()
