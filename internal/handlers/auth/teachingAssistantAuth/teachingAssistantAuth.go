91ae75f59aa0f27a695a2abfd047275d1461552e
= <yeohchongyics@gmail.com>
2024-05-25 02:01:01 +0800

    feat: implement LoginAsTA partially

--- internal/handlers/auth/teachingAssistantAuth/teachingAssistantAuth.go
+++ internal/handlers/auth/teachingAssistantAuth/teachingAssistantAuth.go
@@ -1,14 +1,49 @@
 package teachingAssistantAuth
 
 import (
-	"log"
+	"NUSTuts-Backend/internal/api"
+	"NUSTuts-Backend/internal/auth"
+	data "NUSTuts-Backend/internal/dataaccess"
+	"NUSTuts-Backend/internal/util"
+	"errors"
 	"net/http"
 )
 
-func SignUpAsTA(w http.ResponseWriter, r *http.Request) {
-	log.Default().Println("Sign up")
-}
+// Commented as sign up for TA is not needed
+// func SignUpAsTA(w http.ResponseWriter, r *http.Request) {
+// 	log.Default().Println("Sign up")
+// }
 
 func LoginAsTA(w http.ResponseWriter, r *http.Request) {
-	log.Default().Println("Log in")
+	var payload struct {
+		Email    string `json:"email"`
+		Password string `json:"password"`
+	}
+
+	err := util.ReadJSON(w, r, &payload)
+	if err != nil {
+		util.ErrorJSON(w, err)
+		return
+	}
+
+	ta, err := data.GetTeachingAssistantByEmail(payload.Email)
+	if err != nil {
+		util.ErrorJSON(w, errors.New("TA with this email does not exist!"))
+		return
+	}
+
+	valid, err := util.VerifyPassword(payload.Password, ta.Password)
+	if err != nil || !valid {
+		util.ErrorJSON(w, errors.New("Incorrect Password!"))
+		return
+	}
+
+	authenticatedTA := auth.AuthenticatedUser{
+		ID:    int(ta.ID),
+		Name:  ta.Name,
+		Email: ta.Email,
+		Role:  auth.GetRoleByEmail(ta.Email),
+	}
+
+	util.WriteJSON(w, api.Response{Message: "Login successful", Data: authenticatedTA}, http.StatusOK)
 }
