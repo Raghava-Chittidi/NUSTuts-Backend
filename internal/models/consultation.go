e62bfd942fe03978ae3b2dcd3cba6c61e22c7a69
accountexeregister <yeohchongyics@gmail.com>
2024-06-16 23:39:17 +0800

    feat: add (tutorialId,date,timeslot) multi-field unique constraint for consultation

--- internal/models/consultation.go
+++ internal/models/consultation.go
@@ -4,10 +4,10 @@ import "gorm.io/gorm"
 
 type Consultation struct {
 	gorm.Model
-	TutorialID int `json:"tutorialId" gorm:"not null"`
+	TutorialID int `json:"tutorialId" gorm:"not null uniqueIndex:idx_tutorial_id_date_start_time_end_time"`
 	StudentID int `json:"studentId" gorm:"not null"`
-	Date string `json:"date" gorm:"not null"`
-	StartTime string `json:"startTime" gorm:"not null"`
-	EndTime string `json:"endTime" gorm:"not null"`
+	Date string `json:"date" gorm:"not null uniqueIndex:idx_tutorial_id_date_start_time_end_time"`
+	StartTime string `json:"startTime" gorm:"not null uniqueIndex:idx_tutorial_id_date_start_time_end_time"`
+	EndTime string `json:"endTime" gorm:"not null uniqueIndex:idx_tutorial_id_date_start_time_end_time"`
 	Booked bool `json:"booked" gorm:"not null"`
 }
