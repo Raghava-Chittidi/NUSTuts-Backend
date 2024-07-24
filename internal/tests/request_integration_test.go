8d9d6afcdde8e195b956485377b5bd33903efb5f
accountexeregister <yeohchongyics@gmail.com>
2024-07-25 00:36:44 +0800

    feat: add valid request to join tutoria test

---
+++ internal/tests/request_integration_test.go
@@ -0,0 +1,62 @@
+package tests
+
+import (
+	"NUSTuts-Backend/internal/api"
+	"NUSTuts-Backend/internal/dataaccess"
+	"NUSTuts-Backend/internal/database"
+	"NUSTuts-Backend/internal/models"
+	"fmt"
+	"net/http"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestValidRequestToJoinTutorial(t *testing.T) {
+	testStudent, _, _, err := CreateSingleMockStudentTeachingAssistantAndTutorial()
+	assert.NoError(t, err)
+
+	// Make sure requests table is empty, arbitray where
+	database.DB.Unscoped().Delete(&models.Request{})
+
+	// Current no. of requests in the test db should be 0
+	var count int64
+	database.DB.Table("requests").Count(&count)
+	assert.Equal(t, 0, int(count))
+
+	// Create test tutorial
+	_, err = dataaccess.CreateTutorial("1", "CS2040S", 50)
+	assert.NoError(t, err)
+	// Get the test tutorial
+	testTutorial, err := dataaccess.GetTutorialByClassAndModuleCode("1", "CS2040S")
+	assert.NoError(t, err)
+
+	// Create a request to join tutorial
+	requestToJoinTutPayload := api.RequestToJoinTutorialPayload{
+		StudentID:  int(testStudent.ID),
+		ModuleCode: "CS2040S",
+		ClassNo:    "1",
+	}
+	_, status, err := CreateStudentAuthenticatedMockRequest(requestToJoinTutPayload, fmt.Sprintf("/api/requests/"), "POST", testStudent)
+	assert.NoError(t, err)
+	assert.Equal(t, http.StatusCreated, status)
+
+	// Current no. of requests in the test db should be 1
+	// Get all requests in the test db
+	var requests []*models.Request
+	result := database.DB.Table("requests").Find(&requests)
+	assert.NoError(t, result.Error)
+	// Assert that request count is 1
+	assert.Equal(t, 1, len(requests))
+
+	// Assert that the request is correct
+	assert.Equal(t, int(testStudent.ID), requests[0].StudentID)
+	assert.Equal(t, int(testTutorial.ID), int(requests[0].TutorialID))
+	assert.Equal(t, "pending", requests[0].Status)
+
+	// Cleanup
+	database.DB.Unscoped().Delete(&models.Request{})
+	// Delete the test tutorial
+	database.DB.Unscoped().Delete(&models.Tutorial{})
+	CleanupSingleCreatedStudentTeachingAssistantAndTutorial()
+}
