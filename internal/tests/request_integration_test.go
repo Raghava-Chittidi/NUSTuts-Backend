a1162b6b7dfce60aa4f9584171a3c7a445e22697
accountexeregister <yeohchongyics@gmail.com>
2024-07-25 01:20:29 +0800

    feat: add valid get pending requests test

--- internal/tests/request_integration_test.go
+++ internal/tests/request_integration_test.go
@@ -5,6 +5,7 @@ import (
 	"NUSTuts-Backend/internal/dataaccess"
 	"NUSTuts-Backend/internal/database"
 	"NUSTuts-Backend/internal/models"
+	"encoding/json"
 	"fmt"
 	"net/http"
 	"testing"
@@ -171,4 +172,92 @@ func TestValidRejectRequest(t *testing.T) {
 	// Delete the test tutorial
 	database.DB.Unscoped().Where("1 = 1").Delete(&models.Tutorial{})
 	CleanupSingleCreatedStudentTeachingAssistantAndTutorial()
+}
+
+func TestValidGetPendingRequests(t *testing.T) {
+	testStudentModels := []models.Student{}
+	testDefaultStudent, testDefaultTeachingAssistant, testDefaultTutorial, err := CreateSingleMockStudentTeachingAssistantAndTutorial()
+	assert.NoError(t, err)
+	for _, student := range testStudents {
+		// Create test TeachingAssistant, Student, Tutorial
+		student, err := CreateMockStudent(&student, testDefaultTeachingAssistant, testDefaultTutorial)
+		assert.NoError(t, err)
+		testStudentModels = append(testStudentModels, *student)
+	}
+
+	// Make sure requests table is empty
+	database.DB.Unscoped().Where("1 = 1").Delete(&models.Request{})
+
+	// Current no. of requests in the test db should be 0
+	var count int64
+	database.DB.Table("requests").Count(&count)
+	assert.Equal(t, 0, int(count))
+
+	// Create new TA
+	testTeachingAssistant, err := dataaccess.CreateTeachingAssistant("TEST", "TEST@gmail.com", "1234567890")
+	assert.NoError(t, err)
+
+	// Create test tutorial
+	testTutorial, err := dataaccess.CreateTutorial("1", "CS2040S", int(testTeachingAssistant.ID))
+	assert.NoError(t, err)
+	// Get the test tutorial
+	_, err = dataaccess.GetTutorialByClassAndModuleCode("1", "CS2040S")
+	assert.NoError(t, err)
+
+	for _, student := range testStudentModels {
+		// Create a request to join tutorial
+		requestToJoinTutPayload := api.RequestToJoinTutorialPayload{
+			StudentID:  int(student.ID),
+			ModuleCode: "CS2040S",
+			ClassNo:    "1",
+		}
+		_, status, err := CreateStudentAuthenticatedMockRequest(requestToJoinTutPayload, fmt.Sprintf("/api/requests/"), "POST", &student)
+		assert.NoError(t, err)
+		assert.Equal(t, http.StatusCreated, status)
+	}
+
+	// Current no. of requests in the test db should be number of students
+	// Get all requests in the test db
+	var requests []*models.Request
+	result := database.DB.Table("requests").Find(&requests)
+	assert.NoError(t, result.Error)
+	// Assert that request count is 1
+	assert.Equal(t, len(testStudentModels), len(requests))
+
+	// Get all pending requests
+	res, status, err := CreateTeachingAssistantAuthenticatedMockRequest(nil, fmt.Sprintf("/api/requests/%d", int(testTutorial.ID)), "GET", testTeachingAssistant)
+	assert.NoError(t, err)
+	assert.Equal(t, http.StatusOK, status)
+	// Get response in json
+	var response api.Response
+	err = json.Unmarshal(res, &response)
+	assert.NoError(t, err)
+	resData, _ := json.Marshal(response.Data)
+
+	// Get actual attendance string for the tutorial
+	var requestsResponse []api.RequestResponse
+	err = json.Unmarshal(resData, &requestsResponse)
+	assert.NoError(t, err)
+
+	// Assert that request response length is equal to the number of students
+	assert.Equal(t, len(testStudentModels), len(requestsResponse))
+	// Assert that the request response contains all the student requests and the correct status
+	for i, student := range testStudentModels {
+		assert.Equal(t, student.Name, requestsResponse[i].Name)
+		assert.Equal(t, student.Email, requestsResponse[i].Email)
+	}
+
+	// Cleanup
+	database.DB.Unscoped().Where("1 = 1").Delete(&models.Request{})
+	// Delete the test tutorial
+	database.DB.Unscoped().Where("1 = 1").Delete(&models.Tutorial{})
+
+	// Clean up students, ta, tutorial
+	for _, student := range testStudentModels {
+		CleanupCreatedStudent(&student)
+	}
+	CleanupCreatedStudent(testDefaultStudent)
+	CleanupCreatedTeachingAssistant(testDefaultTeachingAssistant)
+	CleanupCreatedTeachingAssistant(testTeachingAssistant)
+	CleanupCreatedTutorial(testDefaultTutorial)
 }
