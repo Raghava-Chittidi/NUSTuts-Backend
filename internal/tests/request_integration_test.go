b9b868eace11f0148b1e2b2d129eac2baa78103c
accountexeregister <yeohchongyics@gmail.com>
2024-07-25 00:52:20 +0800

    feat: add valid reject request test

--- internal/tests/request_integration_test.go
+++ internal/tests/request_integration_test.go
@@ -116,3 +116,59 @@ func TestValidAcceptRequest(t *testing.T) {
 	database.DB.Unscoped().Where("1 = 1").Delete(&models.Tutorial{})
 	CleanupSingleCreatedStudentTeachingAssistantAndTutorial()
 }
+
+func TestValidRejectRequest(t *testing.T) {
+	testStudent, _, _, err := CreateSingleMockStudentTeachingAssistantAndTutorial()
+	assert.NoError(t, err)
+
+	// Make sure requests table is empty
+	database.DB.Unscoped().Where("1 = 1").Delete(&models.Request{})
+
+	// Current no. of requests in the test db should be 0
+	var count int64
+	database.DB.Table("requests").Count(&count)
+	assert.Equal(t, 0, int(count))
+
+	// Create test tutorial
+	_, err = dataaccess.CreateTutorial("1", "CS2040S", 50)
+	assert.NoError(t, err)
+	// Get the test tutorial
+	_, err = dataaccess.GetTutorialByClassAndModuleCode("1", "CS2040S")
+	assert.NoError(t, err)
+
+	// Create a request to join tutorial
+	requestToJoinTutPayload := api.RequestToJoinTutorialPayload{
+		StudentID:  int(testStudent.ID),
+		ModuleCode: "CS2040S",
+		ClassNo:    "1",
+	}
+	_, status, err := CreateStudentAuthenticatedMockRequest(requestToJoinTutPayload, fmt.Sprintf("/api/requests/"), "POST", testStudent)
+	assert.NoError(t, err)
+	assert.Equal(t, http.StatusCreated, status)
+
+	// Current no. of requests in the test db should be 1
+	// Get all requests in the test db
+	var requests []*models.Request
+	result := database.DB.Table("requests").Find(&requests)
+	assert.NoError(t, result.Error)
+	// Assert that request count is 1
+	assert.Equal(t, 1, len(requests))
+
+	// Reject the request
+	_, status, err = CreateStudentAuthenticatedMockRequest(nil, fmt.Sprintf("/api/requests/%d/reject", requests[0].ID), "PATCH", testStudent)
+	assert.NoError(t, err)
+	assert.Equal(t, http.StatusOK, status)
+
+	// Get the request
+	request, err := dataaccess.GetRequestById(int(requests[0].ID))
+	assert.NoError(t, err)
+
+	// Assert that the request is rejected
+	assert.Equal(t, "rejected", request.Status)
+
+	// Cleanup
+	database.DB.Unscoped().Where("1 = 1").Delete(&models.Request{})
+	// Delete the test tutorial
+	database.DB.Unscoped().Where("1 = 1").Delete(&models.Tutorial{})
+	CleanupSingleCreatedStudentTeachingAssistantAndTutorial()
+}
